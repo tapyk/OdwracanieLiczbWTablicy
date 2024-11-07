947320a3b476c9fd9979193bdd14e2120c301a73
patryk <patyksci@gmail.com>
2024-11-07 11:51:01 +0100

    Dodaj pliki projektów.

---
+++ OdwracanieLiczbTablica/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] tab1;
+int[] tab2;
+
+Console.Write("Podaj rozmiar tablicy: ");
+string rozmiar_str = Console.ReadLine();
+if(int.TryParse(rozmiar_str, out int rozmiar))
+{
+    tab1 = new int[rozmiar];
+    for (int i = 0; i < rozmiar; i++)
+    {
+        Console.Write("Podaj liczbę " + (i + 1) + ": ");
+        string liczba_str = Console.ReadLine();
+        if (int.TryParse(liczba_str, out int liczba))
+        {
+            tab1[i] = liczba;
+        }
+    }
+    tab2 = new int[rozmiar];
+    int j = 0;
+    for (int i = rozmiar - 1; i >= 0; i--)
+    {
+        tab2 [j] = tab1[i];
+        j++;
+    }
+    Console.Write("Tablica: [ ");
+    foreach (var i in tab1)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine("]");
+
+    Console.Write("Tablica odwrócona: [ ");
+    foreach (var i in tab2)
+    {
+        Console.Write(i + " ");
+    }
+    Console.WriteLine("]");
+
+}
